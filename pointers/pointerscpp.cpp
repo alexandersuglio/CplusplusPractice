1fd31e8a8bfe6698a68edf5387e6107d85f4001a
alexandersuglio <alexandersuglio@gmail.com>
2017-10-25 14:07:43 -0400

    added pointer practice

---
+++ pointers/pointerscpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//int cubeByRef(int *n);
+
+int main() {
+
+	//int number = 5;
+	//cout << "Cube by reference function ";
+	//cout << cubeByRef(&number);
+	double array[20];
+
+	cout << sizeof(array);
+
+  cin.get();
+
+}
+/*
+int cubeByRef(int *n) {
+	
+	*n = *n * *n * *n;
+	return *n;
+}
+*/
