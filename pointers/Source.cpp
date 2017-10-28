d332c62cd809ccc7c288da3852cd637e239fba18
Alex <alexandersuglio@gmail.com>
2017-10-27 23:13:56 -0400

    added new pointer practice file

---
+++ pointers/Source.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+//#include <string>
+
+using namespace std;
+
+// Function Prototypes=========================================================
+// Do NOT change the signature of these function prototypes.
+// If you do, your code will not compile with my altered main() function
+// I suggest that you copy the prototypes below and then fill them in.
+// ----------------------------------------------------------------------------
+// Read in a line of text INCLUDING SPACES into a string.
+// You may assume that the input will NOT exceed the maxLength available.
+// Keep in mind that cin stops reading at a whitespace. See page 318.
+void ReadString(char * c, int maxLength);
+
+
+
+// Get the length of the string and store it in length
+// Hint: How does C++ terminate a string? Look for that character!
+void GetStringLength(char * c, int *length);
+
+// PrintString - Just print the string in forward order using cout
+void PrintString(char * const c);
+
+/*
+// PrintStringBackwards - Print the string in reverse order using cout
+void PrintStringBackwards(char * const c);
+
+// Search the string for the test character. Return -1 if not found
+int FindIndexOfCharacter(char * c, char testVal);
+
+// Return a pointer to the character at the index given
+char * GetValueAtIndex(char * const c, int index);
+
+// Return the ascii integer value of the character at requested index
+int GetIntegerValueOfIndex(char * c, int index);
+
+// Print the hexadecimal value of the character at the requested index
+void PrintHexValueAtIndex(char * c, int index);
+
+// Make the entire string uppercase
+void MakeUpperCase(char * c);
+
+// Make the entire string lowercase
+void MakeLowerCase(char * c);
+*/
+// ============================================================================
+
+// DO NOT REMOVE THESE COMMENTS -- comments that start with @ are actually
+// commands to the assessment tool.
+//@include(main)
+//@addRule(commentAll)
+int main()
+{
+	/*
+	const int sizer = 19;
+	char chararray[sizer] = { "jokerdog" };
+	char *charptr;
+
+	//charptr = &chararray[0];
+
+	charptr = chararray;
+
+
+	const int *sizeptr;
+
+	sizeptr = &sizer;
+	
+	cout << strlen(chararray);
+
+	cout << chararray << endl;
+
+	cout << *charptr;
+
+	cout << *sizeptr;
+
+	cout << sizeptr;
+	*/
+
+
+	// Use these variables to test.
+	// SIZE could change so make sure your code works with different sizes.
+	const int SIZE = 80;
+	char ca[SIZE];
+	char * pc = ca;
+	
+	//int fPrints = 0;
+	//int bPrints = 0;
+	//int lengthChecks = 0;
+
+
+	//
+	int length = 0;
+	// Your code below
+	// =========================
+
+	ReadString(pc, SIZE);
+	cout << "Printing====\n";
+	PrintString(pc);
+	cout << "\n";
+	cout << "Done=========" << endl;
+
+
+	cout << "length of ";
+	PrintString(pc);
+	GetStringLength(pc, &length);
+	cout << " is the length of " << length << endl;
+	cout << endl;
+	cin.get();
+
+		
+	
+
+
+
+
+	// =========================
+	// Your code above
+
+	//cout << "Press ENTER";
+	cin.get();
+	//return 0;
+}
+// DO NOT REMOVE THESE COMMENTS -- these comments are actually
+// commands to the assessment tool.
+//@removeRule(commentAll)
+
+void ReadString(char * c, int maxLength)
+{
+
+	cout << "Enter a string less than " << maxLength << " characters." << endl;
+	// Your code here
+	// ==============================
+
+	cin >> c;
+
+	// ==============================
+}
+
+
+
+void GetStringLength(char * c, int *length)
+{
+//*length = static_cast<string>(c).length(); 
+
+	//length;
+		
+*length = strlen(c);
+
+//cout << *length;
+//cout << length;
+	//*lengthptr = 69;
+
+
+
+
+	//*c = c[0];
+
+	//cout << *c;
+//cout << *length;
+	//*length = c.length();
+
+	
+
+}
+
+
+
+
+// You need to provide the rest of the functions
+// See Function Prototypes at the top.
+
+void PrintString(char * const c) {
+
+	cout << c;
+
+}
+
+/*
+void PrintStringBackwards(char * const c) {
+
+}
+
+// Search the string for the test character. Return -1 if not found
+int FindIndexOfCharacter(char * c, char testVal) {
+
+
+}
+
+// Return a pointer to the character at the index given
+char * GetValueAtIndex(char * const c, int index) {
+
+
+}
+
+// Return the ascii integer value of the character at requested index
+int GetIntegerValueOfIndex(char * c, int index) {
+
+
+}
+
+// Print the hexadecimal value of the character at the requested index
+void PrintHexValueAtIndex(char * c, int index) {
+
+
+}
+
+// Make the entire string uppercase
+void MakeUpperCase(char * c) {
+
+
+}
+
+// Make the entire string lowercase
+void MakeLowerCase(char * c) {
+
+
+
+}
+
+*/
--- pointers/pointerscpp.cpp
+++ pointers/pointerscpp.cpp
@@ -1,25 +1,105 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+
 
 using namespace std;
 
-//int cubeByRef(int *n);
+
 
 int main() {
 
-	//int number = 5;
-	//cout << "Cube by reference function ";
-	//cout << cubeByRef(&number);
-	double array[20];
+	int number = 5;
 
-	cout << sizeof(array);
+	int *numptr;
 
-  cin.get();
+	numptr = &number;
+
+	cout << "number = " << number;
+
+	cout << endl;
+
+	cout << "*numptr = " << *numptr;
+
+	cout << endl;
+
+	cout << "&number = " << &number;
+
+	cout << endl;
+
+	cout << "numptr = " << numptr;
+
+
+
+
+	cout << endl << "---------" << endl;
+
+
+	int array[5] = { 1,2,3,4,5 };
+
+	int *arrayptr;
+
+	arrayptr = &array[0];
+
+	cout << "array address is " << array;
+
+	cout << endl;
+
+	cout << "&array address is " << &array;
+
+	cout << endl;
+
+	cout << "&array[0] address is " << &array[0];
+
+	cout << endl;
+
+	cout << "arrayptr address is " << arrayptr;
+
+	cout << endl;
+
+	cout << "&arrayptr address is " << &arrayptr;
+
+	cout << endl;
+
+	cout << "*arrayptr address is " << *arrayptr;
+
+	cout << endl;
+
+	char charArray[5] = { 'r','u','b','y','\0' };
+
+	cout << setw(8) << charArray;
+	cout << endl;
+
+
+	char charArrayii[5] = { "ruby" };
+
+	cout << charArrayii;
+
+	cout << endl;
+
+	//char charArrayiii[2] = { 'ruby' };
+
+	//cout << charArrayiii;
+
+		//char name[5] = { "Alex" };
+
+	cout << "---------------------------------------------" << endl;
+
+
+
+	char name[10];
+
+	cout << "Enter your name below: " << endl;
+	cin.getline(name, 10);
+
+cout << "his name is " << name;
+	//cout << name;
+cout << endl;
+cout << name;
+
+
+
+
+	cin.get();
 
 }
-/*
-int cubeByRef(int *n) {
-	
-	*n = *n * *n * *n;
-	return *n;
-}
-*/
