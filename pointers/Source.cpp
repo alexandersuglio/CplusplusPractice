6ee27c72c6084f359953d79301336caaa1e2be7d
alexandersuglio <alexandersuglio@gmail.com>
2017-10-29 18:52:54 -0400

    added files

--- pointers/Source.cpp
+++ pointers/Source.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -22,14 +22,11 @@ void GetStringLength(char * c, int *length);
 // PrintString - Just print the string in forward order using cout
 void PrintString(char * const c);
 
- 
 // PrintStringBackwards - Print the string in reverse order using cout
 void PrintStringBackwards(char * const c);
  
 // Search the string for the test character. Return -1 if not found
 int FindIndexOfCharacter(char * c, char testVal);
-
-
 
 // Return a pointer to the character at the index given
 char * GetValueAtIndex(char * const c, int index);
@@ -37,7 +34,7 @@ char * GetValueAtIndex(char * const c, int index);
 // Return the ascii integer value of the character at requested index
 int GetIntegerValueOfIndex(char * c, int index);
 
-/*
+
 // Print the hexadecimal value of the character at the requested index
 void PrintHexValueAtIndex(char * c, int index);
 
@@ -46,7 +43,7 @@ void MakeUpperCase(char * c);
 
 // Make the entire string lowercase
 void MakeLowerCase(char * c);
-*/
+ 
 // ============================================================================
 
 // DO NOT REMOVE THESE COMMENTS -- comments that start with @ are actually
@@ -55,99 +52,80 @@ void MakeLowerCase(char * c);
 //@addRule(commentAll)
 int main()
 {
-	/*
-	const int sizer = 19;
-	char chararray[sizer] = { "jokerdog" };
-	char *charptr;
-
-	//charptr = &chararray[0];
-
-	charptr = chararray;
-
-
-	const int *sizeptr;
-
-	sizeptr = &sizer;
 	
-	cout << strlen(chararray);
+// Use these variables to test.
+// SIZE could change so make sure your code works with different sizes.
+const int SIZE = 80;
+char ca[SIZE];
+char * pc = ca;
+int length = 0;
+int fPrints = 0;
+int bPrints = 0;
+int lengthChecks = 0;
 
-	cout << chararray << endl;
+cout << "Enter a string less than " << SIZE << " characters." << endl;
 
-	cout << *charptr;
+ReadString(pc, SIZE);
+cout << "Printing====\n";
+PrintString(pc);
+cout << "\n";
+cout << "Done=========" << endl;
 
-	cout << *sizeptr;
+cout << "length of ";
+PrintString(pc);
+GetStringLength(pc, &length);
+cout << " is " << length << endl;
+cout << endl;
 
-	cout << sizeptr;
-	*/
+cout << "Backwards======\n";
+PrintStringBackwards(pc);
+cout << "\n";
+cout << "Done=========" << endl;
 
+char test = 'X';
+int index = 0;
+index = FindIndexOfCharacter(pc, test);
+cout << test << " index " << index << endl;
 
-	// Use these variables to test.
-	// SIZE could change so make sure your code works with different sizes.
-	const int SIZE = 80;
-	char ca[SIZE];
-	char * pc = ca;
+test = 'a';
+index = 0;
+
+index = FindIndexOfCharacter(pc, test);
+cout << test << " index " << index << endl;
 	
-	//int fPrints = 0;
-	//int bPrints = 0;
-	//int lengthChecks = 0;
+char oldChar = 'S';
+char newChar = 'R';
+char * cPtr;
 
-	char test = 'X';
-	int index = 0;
+index = FindIndexOfCharacter(pc, oldChar);
+cout << oldChar << "-->" << newChar << "\n";
+cPtr = GetValueAtIndex(pc, index);
+*cPtr = newChar;
+PrintString(pc);
+cout << endl;	
 
+for (int i = 0; i < length; ++i)
+{
+int i_ascii = GetIntegerValueOfIndex(pc, i);
+cout << pc[i] << "=" << i_ascii << endl;
+}
+for (int i = 0; i < length; ++i)
+{
+cout << "hex: ";
+PrintHexValueAtIndex(pc, i);
+}
 
-	int length = 0;
-	// Your code below
-	// =========================
+cout << endl;
 
-	ReadString(pc, SIZE);
-	cout << "Printing====\n";
-	PrintString(pc);
-	cout << "\n";
-	cout << "Done=========" << endl;
+MakeUpperCase(pc);
+PrintString(pc);
+cout << endl;
+MakeLowerCase(pc);
+PrintString(pc);
 
-	cout << "length of ";
-	PrintString(pc);
-	GetStringLength(pc, &length);
-	cout << " is " << length << endl;
-	cout << endl;
+cin.get();
+cin.get();
 
-	cout << "Backwards======\n";
-	PrintStringBackwards(pc);
-	cout << "\n";
-	cout << "Done=========" << endl;
-	
-	FindIndexOfCharacter(pc, test);
-	cout << test << " index " << index << endl;
-
-	test = 'a';
-	index = 0;
-	FindIndexOfCharacter(pc, test);
-	cout << test << " index " << index << endl;
-	
-
-
-
-
-
-
-
-
-	//GetValueAtIndex(char * const c, int index);
-	//GetIntegerValueOfIndex(char * c, int index);
-	cin.get();
-
-
-	
-
-
-
-
-	// =========================
-	// Your code above
-
-	//cout << "Press ENTER";
-	cin.get();
-	//return 0;
 }
 // DO NOT REMOVE THESE COMMENTS -- these comments are actually
 // commands to the assessment tool.
@@ -156,107 +134,102 @@ int main()
 void ReadString(char * c, int maxLength)
 {
 
-	cout << "Enter a string less than " << maxLength << " characters." << endl;
+	//cout << "Enter a string less than " << maxLength << " characters." << endl;
 	// Your code here
 	// ==============================
  
-	cin.getline(c, 80, '\n');
+cin.getline(c, maxLength, '\n');
 
 	// ==============================
 }
 
-
-
 void GetStringLength(char * c, int *length)
 {
-//*length = static_cast<string>(c).length(); 
-		
+	
 *length = strlen(c);
-
 }
-
-
-
 
 // You need to provide the rest of the functions
 // See Function Prototypes at the top.
 
 void PrintString(char * const c) {
 
-	cout << c;
+cout << c;
 
 }
 
 
 void PrintStringBackwards(char * const c) {
 
-	for (int i = static_cast<string>(c).length(); i >= 0; i--) {
-		cout << c[i];
-	}
+for (int i = static_cast<string>(c).length() -1; i >= 0; i--) {
+cout << c[i];
+}
 
 }
 
 // Search the string for the test character. Return -1 if not found
 int FindIndexOfCharacter(char * c, char testVal) {
 	
-	for (int i = 0; i < static_cast<string>(c).length(); i++) {
+
+for (int i = 0; i < static_cast<string>(c).length(); i++) {
 		
-		/*
-		if (testVal == c[i]){
-			return c[i];
-			//cout << "I love ruby ";
-		}
-		else {
-			return -1;
-		}
-		*/
-			return 13;
-	}
+if (testVal == c[i])
+return i;
+			
+}
 
-	//return 0;
-
+return -1;
 }
 
 
 // Return a pointer to the character at the index given
 char * GetValueAtIndex(char * const c, int index) {
+	cout << "ruby";
 
-	return "a";
 
+	//for (int i = 0; i < strlen(c); i++) {
+	//if (c[index] == )
+	//	char p = c;
+		return c;
+	//}
+
+
+
+//int i = 0;
+//char *p = c;
+//while (i++ != index)
+//	p++;
+//for (int i = 0; i < strlen(c); i++)
+//if (c[i] == index)
+//<string>(p);
+//return p;
+	
 }
 
 // Return the ascii integer value of the character at requested index
 int GetIntegerValueOfIndex(char * c, int index) {
-
-	return 6;
+return int(c[index]);
 }
 
-
-
-
-
-
-/*
 // Print the hexadecimal value of the character at the requested index
 void PrintHexValueAtIndex(char * c, int index) {
 
-
-
+cout << hex << int(c[index]) << endl;
 }
-
 
 
 // Make the entire string uppercase
 void MakeUpperCase(char * c) {
-
-
+for (int i = 0; i < static_cast<string>(c).length(); i++) {
+c[i] = toupper(c[i]);
+}
 }
 
 // Make the entire string lowercase
 void MakeLowerCase(char * c) {
 
-
-
+for (int i = 0; i < static_cast<string>(c).length(); i++) {
+c[i] = tolower(c[i]);
 }
-
-*/
+}
+ 
