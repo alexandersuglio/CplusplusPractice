0b0aa7510ebf3f3ce8de6c9ffb0937c3e647331f
Alex Suglio <alexandersuglio@gmail.com>
2018-02-13 22:46:20 -0500

    added recent

---
+++ COP3330/String and Char practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    
+    string string1("ruby");
+    cout << "string string1(''ruby''): " << string1.size() << endl;
+    
+    string dog = "I love dogs";
+    cout << "string dog: " << dog << endl;
+    
+    char line = 'r';
+    cout << "char line: " << line << endl;
+    
+    //string literal
+    //automatically added null
+    char name[]="ruby russell";
+    cout << "char name[]: " << name[12]<< endl;
+    cout << "char name[]: " << strlen(name) << endl;
+    
+    char first[]= {'r', 'o', 'v', '\0', 'j', 'o', '\0'};
+    cout << "first[]:" << strlen(first) << endl;
+    
+}
